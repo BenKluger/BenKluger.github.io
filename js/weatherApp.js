fb43e29f214a6639327536fe3c7e2233c45ec620
Ben Kluger <69221395+BenKluger@users.noreply.github.com>
2021-11-04 00:32:04 -0400

    Done.

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -1,21 +1,51 @@
+function cToF(celsius) { //Better Display
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  return cToFahr;
+}
+
 $(document).ready(function () {
   //when it's finished loading
 
-  function weather() {
+  //Get Location
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(pos){
+    var lat = pos.coords.latitude; //to find the correct path you can console.log out pos and find the correct path
+    var long = pos.coords.longitude;
+    weather(lat, long)
+  }
+
+  function error(){
+    console.log("Error");
+  }
+
+  function weather(lat, long) {
     var URL =
-      "https://fcc-weather-api.glitch.me/api/current?lat=53.70&lon=-1.24";
+      `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
     $.getJSON(URL, function (data) {
       console.log(data);
       updateDOM(data);
     });
   }
 
-  weather();
+
+
 
   function updateDOM(data) {
     var city = data.name;
-    var temp = data;
+    var temp = Math.round(cToF((data.main.temp))); //Convert the weather to Fahrenheit
+    var desc = data.weather[0].description;
+    var icon = data.weather[0].icon;
+    var feeling = Math.round(cToF((data.main.feels_like)));
+    var high = Math.round(cToF((data.main.temp_max)));
+    var low = Math.round(cToF((data.main.temp_min)));
 
+    $('#temp').html(temp);
     $("#city").html(city); //replace the id in the page with city with the data
+    $('#desc').html(desc);
+    $('#icon').attr('src', icon);
+    $('#feelsLike').html(`Feels Like: ${feeling}&#176;` );
+    $('#highNlow').html(`High: ${high}&#176;   Low: ${low}&#176;`);
   }
 });
